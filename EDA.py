065b03205a645566793e09675ebf62767e2668e1
Ting Lan <31231595+tingtingting118@users.noreply.github.com>
2023-08-28 19:27:20 +0000

    adding the first couple of notes to test

---
+++ EDA.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statistics
+
+
+
+#Importing the datasets
+data =pd.read_csv("LoansTrainingSetV2.csv", low_memory = False)
+
+data.head()
+
+
